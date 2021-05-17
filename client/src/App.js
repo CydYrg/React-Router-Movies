a70aca43be9700f86273a725fc8a658606899a2f
CydYrg <ceyda_yurga@hotmail.com>
2021-05-17 22:15:45 +0100

    additional changes on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,24 +14,25 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => setMovieList(res.data))
+        .then(response => setMovieList(response.data))
     }
+    getMovies();
 
-          // Study this response with a breakpoint or log statements
+
+          // Study this response with a breakpoint or log statements TRIED!!
           // and set the response data as the 'movieList' slice of state
         })
         .catch(error => {
           console.error('Server Error', error);
         });
+
     }
-    getMovies();
-  
+   
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
-
-  return (
+ return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
